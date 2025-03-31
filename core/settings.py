3bb04dfb65ea27f9f80b70e6998501f8d44dcef6
madsonmendes87 <madsonmendes87@gmail.com>
2025-03-31 16:40:51 -0300

    settings pt-br

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u%1^tt&)0k8nwiw3mnr%ubw(sj)0o@3qrl0vb9(%g1zfp5*r0^'
+SECRET_KEY = 'django-insecure-d+w#khrj+s)oi_3rbf%17@$one)czx%@v1$o7hf5@9-%i6&@2^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
